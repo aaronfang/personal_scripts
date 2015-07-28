508ce7df8fb46f3f5f569c4a897a651bc90325ae
Aaron Fang <fangzheng003@gmail.com>
2015-07-28 16:17:25 +0800

    Update af_assemblyPrep.py

--- scripts/af_assemblyPrep.py
+++ scripts/af_assemblyPrep.py
@@ -10,4 +10,11 @@ scl = pm.xform(curSel,ws=1,s=1,q=1)
 locNd = pm.spaceLocator(n=(curSel[0]+'_LOC'),p=(0,0,0))
 pm.xform(locNd,s=scl,t=(trans[0],trans[1],trans[2]),ro=rot)
 
-# 
+# Parent the top group to the locator.
+# Add attribute to store the trans/rot/scale information.
+pm.parent(curSel[0],locNd)
+pm.addAttr(locNd,ln='scal_info',at='double3')
+pm.addAttr(locNd,p='scal_info',ln='scale_infoX',at='double')
+pm.addAttr(locNd,p='scal_info',ln='scale_infoY',at='double')
+pm.addAttr(locNd,p='scal_info',ln='scale_infoZ',at='double')
+pm.setAttr((locNd+'.scal_info'),0,0,0,type='double3')
