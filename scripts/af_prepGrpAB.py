1ef45303c496f613af64db890df8869dd12b67c1
Aaron Fang <fangzheng003@gmail.com>
2015-07-30 16:47:12 +0800

    Create af_prepGrpAB.py

---
+++ scripts/af_prepGrpAB.py
@@ -0,0 +1,41 @@
+# ------------------------------------------
+# af_prepGrpA.py
+# Summary: This script allows user to relocate the pivot point of a asset.
+# 1, Make outliner structure as famity name.
+# 2, Select the group node in asset level.
+# 3, Run the following script.
+
+import pymel.core as pm
+import maya.mel as mm
+
+# add '_grp' to the top node
+curSel = pm.ls(sl=True,type='transform')[0]
+newNm = curSel + '_grp'
+pm.rename(curSel,newNm)
+
+# center pivot
+mm.eval('CenterPivot')
+
+# freeze transform
+import pymel.core as pm
+pm.makeIdentity(apply=1,t=1,r=1,s=1,n=0,pn=0)
+
+# grouping
+assetNm = curSel.split('_')[0]
+trans = pm.xform(curSel,ws=1,piv=1,q=1)
+rot = pm.xform(curSel,ws=1,ro=1,q=1)
+scl = pm.xform(curSel,ws=1,s=1,q=1)
+gpNd = pm.group(n=assetNm,em=1)
+pm.xform(gpNd,s=sclAfter,t=trans[0:3],ro=rot)
+
+# display local rotation axis
+mm.eval('ToggleLocalRotationAxes')
+
+# isolate assets
+mm.eval('$currentPanel = `getPanel -withFocus`;enableIsolateSelect $currentPanel true;')
+
+# ------------------------------------------
+# af_prepGrpB.py
+# Summary: This script parent pivot group back to the assets structure.
+
+# 
