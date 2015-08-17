583cf7eafe66d61999a467c88f35e5ea9a6ee42e
Aaron Fang <fangzheng003@gmail.com>
2015-08-17 10:21:47 +0800

    Update af_atlasmapTGL.py

--- scripts/af_atlasmapTGL.py
+++ scripts/af_atlasmapTGL.py
@@ -1,15 +1,12 @@
 # This script will switch UV Set between "map1" and "atlasmap".
 # Useage:
-#   Select meshes which have both map1 and atlas map. Run this script
- 
+# Select meshes which have both map1 and atlas map. Run this script
 import pymel.core as pm
- 
 spnd = pm.ls(sl=True, fl=True, dag=True, type='shape')
 curUVset = pm.polyUVSet(spnd[0],q=True, currentUVSet=True)
- 
 if curUVset[0] == 'map1':
-    for sp in spnd:
-        pm.polyUVSet(sp, currentUVSet=True, uvSet="atlasmap");
-elif curUVset[0] == 'atlasmap':
-    for sp in spnd:
-        pm.polyUVSet(sp, currentUVSet=True, uvSet="map1");
+	for sp in spnd:
+		pm.polyUVSet(sp, currentUVSet=True, uvSet="atlasUV")
+elif curUVset[0] == 'atlasUV':
+	for sp in spnd:
+		pm.polyUVSet(sp, currentUVSet=True, uvSet="map1")
