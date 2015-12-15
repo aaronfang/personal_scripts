e0da76c6b1ca6d1de744a17d08d2c36e7f93b150
Aaron Fang <fangzheng003@gmail.com>
2015-12-15 10:34:38 +0800

    Update import_multiple_object_files.py

--- external/import_multiple_object_files.py
+++ external/import_multiple_object_files.py
@@ -1,5 +1,3 @@
-import maya.cmds as cmds
-
 def import_multiple_object_files():
     files_to_import = cmds.fileDialog2(fileFilter =  '*.obj', dialogStyle = 2, caption = 'import multiple object files', fileMode = 4)
     for file_to_import in files_to_import:
@@ -7,5 +5,7 @@ def import_multiple_object_files():
         object_name = names_list[-1].replace('.obj', '') 
         returnedNodes = cmds.file('%s' % file_to_import, i = True, type = "OBJ", rnn=True, ignoreVersion = True, options = "mo=0",  loadReferenceDepth  = "all"  )
         cmds.rename( returnedNodes[0], object_name)
-
+        for nd in returnedNodes:
+			if '|' in nd and 'Shape' not in nd:
+				cmds.rename(nd, object_name)
 import_multiple_object_files()
