296ce982fe324559cbcd1d48c59f0ef155165ec9
Aaron Fang <aaronfang@users.noreply.github.com>
2015-12-30 10:51:19 +0800

    add test line

--- scripts/afXform.py
+++ scripts/afXform.py
@@ -10,3 +10,4 @@ grpNmb = cmds.ls(sl=True,type='transform')[0]
 cmds.xform(grpNmb,ws=1,t=(grpTa[0],grpTa[1],grpTa[2]))
 cmds.xform(grpNmb,r=1,ro=(grpRa[0],grpRa[1],grpRa[2]))
 # cmds.xform(grpNmb,s=1,sp=(grpSa[0],grpSa[1],grpSa[2]))
+# test
