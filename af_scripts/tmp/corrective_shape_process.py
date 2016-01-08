f68b9dbec434fd8823dbce0b31b165dc946397be
Aaron Fang <fangzheng003@gmail.com>
2016-01-08 13:54:02 +0800

    Create corrective_shape_process.py

---
+++ af_scripts/tmp/corrective_shape_process.py
@@ -0,0 +1,15 @@
+# R_mouth_up_right_geo
+# R_mouth_down_left_geo
+# R_mouth_up_left_geo
+# R_mouth_down_right_geo
+# L_mouth_down_right_geo
+# L_mouth_up_right_geo
+# L_mouth_down_left_geo
+# L_mouth_up_left_geo
+
+# 1, rename current mouth corrective shapes and unparent from mouth_shapes group
+# 2, dial ctrl to max value(e.g.tx=1,ty=-1)
+# 3, hit the button named "Create Corrective Target from Current Pose"
+# 4, select org shape then cur shape, create blendshape then delete histroy
+# 5, hit button named "Generate Corrective Shapes and Connect to Rig"
+# 6, test load pose if worked. Then hide corrective shape and do next.
