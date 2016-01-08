722ae82ddeaf1ed24b82a9deccc18ebe5125d95f
Aaron Fang <fangzheng003@gmail.com>
2016-01-08 13:54:16 +0800

    Create freezeVtxTransform.py

---
+++ af_scripts/tmp/freezeVtxTransform.py
@@ -0,0 +1,9 @@
+import pymel.core as pm
+
+def freezeVtxTransformation():
+	curSel = pm.ls(sl=1,fl=1)
+	for sel in curSel:
+		sel = sel.getShape()
+		pm.polyMoveVertex(sel,ch=0)
+		pm.select(d=True)
+freezeVtxTransformation()
