08b3056634452c08849d86299c3a54c1b69a6061
Aaron Fang <fangzheng003@gmail.com>
2016-01-11 22:07:11 +0800

    Update lineUpUVs.py

--- af_scripts/uv/lineUpUVs.py
+++ af_scripts/uv/lineUpUVs.py
@@ -1,6 +1,7 @@
 import pymel.core as pm
 import math
-def lineUpUVs():
+
+def lineUpU():
 	sels = pm.ls(sl=1)
 	gap = 0.003
 	for i, x in enumerate(sels):
@@ -13,4 +14,17 @@ def lineUpUVs():
 		else:
 			pm.polyEditUV(u=-buv[0][0]+(w*i+gap*(i+1)),v=-buv[1][0]+gap)
 	pm.select(sels,r=1)
-lineUpUVs()
+
+def lineUpV():
+	sels = pm.ls(sl=1)
+	gap = 0.003
+	for i, x in enumerate(sels):
+		x=x.getShape()
+		pm.select('{0}.map[:]'.format(x), r=1)
+		buv = pm.polyEvaluate(x,b2=1)
+		w = abs(buv[1][1] - buv[1][0])
+		if i==0:
+			pm.polyEditUV(v=-buv[1][1]-(gap*(i+1)),u=-buv[0][0]+gap)
+		else:
+			pm.polyEditUV(v=-buv[1][1]-(w*i+gap*(i+1)),u=-buv[0][0]+gap)
+	pm.select(sels,r=1)
