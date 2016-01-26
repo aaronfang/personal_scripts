f65babfd886315036c834acef9c361bd08bd3de7
Aaron Fang <fangzheng003@gmail.com>
2016-01-26 19:38:52 +0800

    Update randSelect.py

--- af_scripts/tmp/randSelect.py
+++ af_scripts/tmp/randSelect.py
@@ -1,23 +1,33 @@
-import random as rd
+# Shuffle a list then separate it into certain parts.
+# Usage: randomSplitList().randListFunc(list,parts)
+# will return a list contains separated parts for use.
+import pymel.core as pm
+import random
 
-list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+class randomSplitList(object):
+	def __init__(self):
+		self.list,self.cols,self.randList,self.newList = list,cols,randList,newList
+		self.list=[]
+		self.cols=1
+		self.randList=[]
+		self.newList=[]
 
-randList = list
-#print randList
+	def splitList(self,list,cols):
+		start = 0
+		newList = []
+		for i in xrange(cols):
+			stop = start + len(list[i::cols])
+			newList.append(list[start:stop])
+			start = stop
+		return newList
 
-div=3
-
-listSize=len(list)
-#print listSize
-
-numForOnePart=listSize/div
-#print numForOnePart
-
-rd.shuffle(randList)
-
-#print randList
+	def randListFunc(self,list,cols):
+		randList = list
+		random.shuffle(randList)
+		newList = self.splitList(randList,cols)
+		return newList
 
 
-print [randList[i::3] for i in range(3)]
-  
-print randList
+
+pm.select(randomSplitList().randListFunc(a,4)[0],r=1)
+a=pm.ls(sl=1,fl=1)
