6956995ab5661cb5c3ca58722734e4097ce85ac6
Aaron Fang <fangzheng003@gmail.com>
2016-01-29 00:11:47 +0800

    Update randSelect.py

--- af_scripts/tmp/randSelect.py
+++ af_scripts/tmp/randSelect.py
@@ -6,11 +6,11 @@ import random
 
 class randomSplitList(object):
 	def __init__(self):
+		list=[]
+		cols=1
+		randList=[]
+		newList=[]
 		self.list,self.cols,self.randList,self.newList = list,cols,randList,newList
-		self.list=[]
-		self.cols=1
-		self.randList=[]
-		self.newList=[]
 
 	def splitList(self,list,cols):
 		start = 0
@@ -37,15 +37,18 @@ class randListUI(object):
 		
 		w=280
 		
-		window=pm.window("mainWin",t="Shuffle Select Tool",s=0,mb=1,mnb=0,mxb=0,rtf=1,w=w)
+		window=pm.window("mainWin",t="Shuffle Slice Tool",s=0,mb=1,mnb=0,mxb=0,rtf=1,w=w)
 		pm.columnLayout("mainColumn",p="mainWin",columnAttach=('both', 2), rowSpacing=10, columnWidth=w)
-		pm.text(l="Slice selection into random parts")
-		pm.intSliderGrp("colsSlider",p="mainColumn",cw3=(30,45,50),field=True,label='Parts',minValue=1,maxValue=5,fieldMinValue=1,value=1,step=1)
+		pm.text("warningText",p="mainColumn",l="---===Select something to slice===---",al="center")
+		pm.intSliderGrp("colsSlider",p="mainColumn",cw3=(30,45,50),field=True,label='Parts',minValue=1,fieldMinValue=1,value=1,fs=1,ss=1)
 		pm.radioButtonGrp("radioBtnGrp",l=" ",labelArray2=['List', 'Group'],cw3=[60,90,80],numberOfRadioButtons=2,sl=1)
 		
-		pm.textScrollList("listScroll",p="mainColumn",numberOfRows=5, allowMultiSelection=False,sc=)
+		pm.textScrollList("listScroll",p="mainColumn",numberOfRows=5, allowMultiSelection=False,sc=self.selPart)
+		pm.popupMenu("listPopUp",p="listScroll")
+		pm.menuItem(p="listPopUp",l="Create Group for the parts",c=self.groupPartsFunc)
 		
-		pm.button(p="mainColumn",l="Shuffle")
+		pm.button(p="mainColumn",l="Shuffle Slice",c=self.shuffleSlice)
+		
 
 		pm.showWindow("mainWin")
 	
@@ -55,16 +58,41 @@ class randListUI(object):
 		if len(getSel)>=1 and len(getSel)>=cols:
 			array = getSel
 			self.result = randomSplitList().randListFunc(array,cols)
-			if pm.radioButtonGrp("radioBtnGrp",q=1,sl=1) == "List":
+			if pm.radioButtonGrp("radioBtnGrp",q=1,sl=1) == 1:
+				pm.text("warningText",e=1,l="---===List All Parts===---")
 				pm.textScrollList("listScroll",e=1,ra=1)
 				for i in range(0,len(self.result)):
-					pm.textScrollList("listScroll",e=1,append="{0},{1}".format("Part-",(i+1)))
+					pm.textScrollList("listScroll",e=1,append="{0}{1}".format("ShuffledPart_",(i+1)))
+			elif pm.radioButtonGrp("radioBtnGrp",q=1,sl=1) == 2:
+				pm.textScrollList("listScroll",e=1,ra=1)
+				allGrps=[]
+				for i in range(0,len(self.result)):
+					grp = pm.group(n=("{0}{1}".format("ShuffledPart_",(i+1))),em=1)
+					pm.parent(self.result[i],grp)
+					allGrps.append(grp)
+				pm.select(allGrps,r=1)
+		elif len(getSel)==0:
+			pm.textScrollList("listScroll",e=1,ra=1)
+			pm.text("warningText",e=1,l="---===Select Something!!!===---")
+		elif len(getSel)>0 and len(getSel)<cols:
+			pm.textScrollList("listScroll",e=1,ra=1)
+			pm.text("warningText",e=1,l="---===Too Much Parts!!!===---")
 	
 	def selPart(self,*args):
-		if pm.textScrollList("listScroll",q=1,si=1)
+		curSelInList = pm.textScrollList("listScroll",q=1,si=1)
+		i = curSelInList[0][-1]
+		if i.isdigit():
+			pm.select(self.result[(int(i)-1)],r=1)
+		
+	def groupPartsFunc(self,*args):
+		allGrps=[]
+		if len(pm.textScrollList("listScroll",q=1,ai=1))!=0:
+			if len(self.result)==len(pm.textScrollList("listScroll",q=1,ai=1)):
+				for i in range(0,len(self.result)):
+					grp = pm.group(n=("{0}{1}".format("ShuffledPart_",(i+1))),em=1)
+					pm.parent(self.result[i],grp)
+					allGrps.append(grp)
+				pm.select(allGrps,r=1)
+
+		
 randListUI()._UI()
-
-
-
-pm.select(randomSplitList().randListFunc(a,4)[0],r=1)
-a=pm.ls(sl=1,fl=1)
