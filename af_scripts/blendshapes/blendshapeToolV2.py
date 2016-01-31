27fd72df5f47c44e0133f8fc0c07251a9a1fc19f
Aaron Fang <fangzheng003@gmail.com>
2016-02-01 00:08:39 +0800

    Create blendshapeToolV2.py

---
+++ af_scripts/blendshapes/blendshapeToolV2.py
@@ -0,0 +1,24 @@
+# blendshape tools
+# - import and replace current target shapes
+# - export target shapes as separate objs
+# - create blendshape based on selection(src/target) and lock the src
+# - display blendshape target sliders for selected src geometries
+# - add/remove blendshape targets
+# - add/remove in-between shapes based on naming
+# - update in-between positions by updating naming
+# - modify in-between shape in solo mode
+# - toggle in-between shape with before/after
+# - export high-res weight map
+# - update target shapes by blendshape weights
+# - check model symmetry
+# - mirror half(continusily model)
+# - mirror half(separated model)
+# - freeze transform for all vertices
+# - update all target shapes and in-between shapes based on naming
+# - paint weight enhance functions:
+#     - reverse value
+#     - add/reduce value for non-zero areas
+#     - mark all border as 0
+#     - weightmap list for save/load
+#     - increase/decrease area
+#     - mirror weights
