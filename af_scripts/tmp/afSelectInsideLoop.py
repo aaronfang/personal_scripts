3483564bd0a5328965fea17eca188dd2813cab01
Aaron Fang <aaronfang@users.noreply.github.com>
2016-02-10 00:47:36 +0800

    add select inside of loop idea.

---
+++ af_scripts/tmp/afSelectInsideLoop.py
@@ -0,0 +1,24 @@
+'''
+select inside the loop:
+	- select an edge/face/vert loop and a vtx/edge/face inside of loop
+	- get loop and convert to edges
+	- create a temp uvset and give a planer map
+	- separate uvs into shells based on edge loop
+	- select the uv shell contains the last selection and convert to face
+	- remove the temp uvset
+
+'''
+import maya.cmds as cmds
+#import afCore.cmds as ac
+get_sel = cmds.ls(sl=True,fl=True)
+last_comp = get_sel[-1]
+loop_comp = get_sel[:-1]
+if ac.is_comp(get_sel) == "face":
+	edge_loop = ac.convert_comp(f=loop_comp,el=1)
+	cmds.uvSet("temp_uvset_for_selection",create=1)
+	cmds.cutUV(edge_loop)
+	cmds.select(last_comp,r=True)
+	cmds.toShell()
+	cmes.toface()
+	cmds.delete("temp_uvset_for_selection",uvSet=1)
+	
