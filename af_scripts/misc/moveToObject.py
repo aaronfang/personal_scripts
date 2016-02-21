c09c9942ace8e64cc994a47289972f7c5ef73242
Aaron Fang <fangzheng003@gmail.com>
2016-02-21 22:30:56 +0800

    update code

--- af_scripts/misc/moveToObject.py
+++ af_scripts/misc/moveToObject.py
@@ -2,19 +2,16 @@
 
 import pymel.core as pm
 
+
 def move_to_object():
-	get_sel = pm.ls(sl=1,fl=1)
-	if len(get_sel) == 2:
-		src = get_sel[1]
-		target = get_sel[0]
-		src_oc_x = pm.objectCenter(src,x=1)
-		src_oc_y = pm.objectCenter(src,y=1)
-		src_oc_z = pm.objectCenter(src,z=1)
-		target_oc_x = pm.objectCenter(target,x=1)
-		target_oc_y = pm.objectCenter(target,y=1)
-		target_oc_z = pm.objectCenter(target,z=1)
-		src_oc = [src_oc_x,src_oc_y,src_oc_z]
-		target_oc = [target_oc_x,target_oc_y,target_oc_z]
-		vector = (src_oc_x-target_oc_x),(src_oc_y-target_oc_y),(src_oc_z-target_oc_z)
-		pm.xform(target,t=vector,r=1)
+    get_sel = pm.ls(sl=1, fl=1)
+    if len(get_sel) == 2:
+        src = get_sel[1]
+        target = get_sel[0]
+        src_oc = pm.objectCenter(src)
+        target_oc = pm.objectCenter(target)
+        vector = (src_oc[0] - target_oc[0]), (src_oc[1] - target_oc[1]), (src_oc[2] - target_oc[2])
+        pm.xform(target, t=vector, r=1)
+
+
 move_to_object()
