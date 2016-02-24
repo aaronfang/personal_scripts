9458c13d78f6eb80c2802580389f0959678f5f08
Aaron Fang <fangzheng003@gmail.com>
2016-02-24 23:48:44 +0800

    reformatting in pycharm

--- af_scripts/tmp/addRmDeformerInfluence.py
+++ af_scripts/tmp/addRmDeformerInfluence.py
@@ -1,10 +1,9 @@
 # Add objects from selected deformer
-curSel = cmds.ls(sl=1,fl=1)
+curSel = cmds.ls(sl=1, fl=1)
 for sel in curSel[0:-1]:
-	cmds.deformer(curSel[-1],e=1,g=sel)
-
+    cmds.deformer(curSel[-1], e=1, g=sel)
 
 # Remove objects from selected deformer
-curSel = cmds.ls(sl=1,fl=1)
+curSel = cmds.ls(sl=1, fl=1)
 for sel in curSel[0:-1]:
-	cmds.deformer(curSel[-1],e=1,g=sel,rm=1)
+    cmds.deformer(curSel[-1], e=1, g=sel, rm=1)
