d60debb2f07b25faec87be74116e44172af3e4b5
Aaron Fang <fangzheng003@gmail.com>
2016-02-27 23:33:14 +0800

    update code

--- af_scripts/uv/AlignUVs.py
+++ af_scripts/uv/AlignUVs.py
@@ -213,10 +213,11 @@ import maya.cmds as cmds
 import math
 
 sels = cmds.ls(os=1)
-gap_w = 0.1
+gap_w = 0.07
 gap_h = 0.01
 initGap = 0.003
 UDIM_limit = 10
+fst_in_UDIM = 0
 
 for i, x in enumerate(sels):
     #x = x.getShape()
@@ -241,10 +242,11 @@ for i, x in enumerate(sels):
         # if shell U is out of UDIM
         if UDIM-UDIM_last==1:
             new_u = -(cmds.polyEvaluate(x, b2=1)[0][0]-initGap)+(UDIM-2)
-            new_v = -(cmds.polyEvaluate(x, b2=1)[1][0]-cmds.polyEvaluate(sels[0:i], b2=1)[1][1])+gap_h
+            new_v = -(cmds.polyEvaluate(x, b2=1)[1][0]-cmds.polyEvaluate(sels[fst_in_UDIM:i], b2=1)[1][1])+gap_h
             cmds.polyEditUV(u=new_u, v=new_v)
-            if int((math.floor(cmds.polyEvaluate(x, b2=1)[0][1])+1)+(math.floor(cmds.polyEvaluate(x, b2=1)[1][1])*10))-UDIM_last>1:
-                new2_u = -(cmds.polyEvaluate(x, b2=1)[0][0]-initGap)+(UDIM_new-UDIM_limit)
+            if int((math.floor(cmds.polyEvaluate(x, b2=1)[0][1])+1)+(math.floor(cmds.polyEvaluate(x, b2=1)[1][1])*10))-UDIM_last==10:
+                fst_in_UDIM = i
+                new2_u = -(cmds.polyEvaluate(x, b2=1)[0][0]-initGap)+(int((math.floor(cmds.polyEvaluate(x, b2=1)[0][1])+1)+(math.floor(cmds.polyEvaluate(x, b2=1)[1][1])*10))-UDIM_limit)
                 new2_v = -cmds.polyEvaluate(x, b2=1)[1][0] + initGap
                 cmds.polyEditUV(u=new2_u, v=new2_v)
 
