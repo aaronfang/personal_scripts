01d98f2004540380baece638f3d54af2f007ab78
Aaron Fang <fangzheng003@gmail.com>
2016-02-28 19:21:45 +0800

    update alignUVs to UDIM.

--- af_scripts/uv/AlignUVs_V2.py
+++ af_scripts/uv/AlignUVs_V2.py
@@ -1,26 +1,26 @@
 import maya.cmds as cmds
 import math
 
-
 def lineup_U_in_UDIM():
     sels = cmds.ls(os=1)
-    gap_w = 0.07
-    gap_h = 0.01
+    gap_w = 0.01
+    gap_h = 0.05
     initGap = 0.003
     UDIM_limit = 10
     fst_in_UDIM = 0
 
     for i, x in enumerate(sels):
+        #x = x.getShape()
         cmds.select('{0}.map[:]'.format(x), r=1)
 
-        if i == 0:
+        if i==0:
             # move to the init place
             cmds.polyEditUV(u=-cmds.polyEvaluate(x, b2=1)[0][0] + initGap, v=-cmds.polyEvaluate(x, b2=1)[1][0] + initGap)
 
-        elif i >= 1:
+        elif i>=1:
             # get the size of the last shell
             w_last = cmds.polyEvaluate(sels[i-1], b2=1)[0][1]-cmds.polyEvaluate(sels[i-1], b2=1)[0][0]
-            # h_last = cmds.polyEvaluate(sels[i-1], b2=1)[1][1]-cmds.polyEvaluate(sels[i-1], b2=1)[1][0]
+            h_last = cmds.polyEvaluate(sels[i-1], b2=1)[1][1]-cmds.polyEvaluate(sels[i-1], b2=1)[1][0]
             # calc the distance to the last shell
             dist_u = cmds.polyEvaluate(x, b2=1)[0][0]-cmds.polyEvaluate(sels[i-1], b2=1)[0][0]
             dist_v = cmds.polyEvaluate(x, b2=1)[1][0]-cmds.polyEvaluate(sels[i-1], b2=1)[1][0]
@@ -34,10 +34,16 @@ def lineup_U_in_UDIM():
                 new_u = -(cmds.polyEvaluate(x, b2=1)[0][0]-initGap)+(UDIM-2)
                 new_v = -(cmds.polyEvaluate(x, b2=1)[1][0]-cmds.polyEvaluate(sels[fst_in_UDIM:i], b2=1)[1][1])+gap_h
                 cmds.polyEditUV(u=new_u, v=new_v)
-                # if shell V is out of UDIM
+                #if shell V is out of UDIM
                 if int((math.floor(cmds.polyEvaluate(x, b2=1)[0][1])+1)+(math.floor(cmds.polyEvaluate(x, b2=1)[1][1])*10))-UDIM_last==10:
                     fst_in_UDIM = i
                     new2_u = -(cmds.polyEvaluate(x, b2=1)[0][0]-initGap)+(int((math.floor(cmds.polyEvaluate(x, b2=1)[0][1])+1)+(math.floor(cmds.polyEvaluate(x, b2=1)[1][1])*10))-UDIM_limit)
                     new2_v = -cmds.polyEvaluate(x, b2=1)[1][0] + initGap
                     cmds.polyEditUV(u=new2_u, v=new2_v)
-    cmds.select(sels, r=1)
+            elif int((math.floor(cmds.polyEvaluate(x, b2=1)[0][1])+1)+(math.floor(cmds.polyEvaluate(x, b2=1)[1][1])*10))-UDIM_last==10:
+                fst_in_UDIM = i
+                new2_u = -(cmds.polyEvaluate(x, b2=1)[0][0]-initGap)+(int((math.floor(cmds.polyEvaluate(x, b2=1)[0][1])+1)+(math.floor(cmds.polyEvaluate(x, b2=1)[1][1])*10))-UDIM_limit)
+                new2_v = -cmds.polyEvaluate(x, b2=1)[1][0] + initGap
+                cmds.polyEditUV(u=new2_u, v=new2_v)
+    cmds.select(sels,r=1)
+lineup_U_in_UDIM()
