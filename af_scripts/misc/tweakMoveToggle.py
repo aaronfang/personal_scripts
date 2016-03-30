41697c86b0a567c49bd6375b8ad10b9984c91347
Aaron Fang <fangzheng003@gmail.com>
2016-03-30 23:26:38 +0800

    Update tweakMoveToggle.py

--- af_scripts/misc/tweakMoveToggle.py
+++ af_scripts/misc/tweakMoveToggle.py
@@ -7,9 +7,23 @@ import maya.mel as mel
 
 if cmds.selectMode(q=True,co=True):
     cmds.selectMode(o=True)
+    mel.eval('setToolTo $gSelect')
     mel.eval('dR_DoCmd("selectModeDisableTweakMarquee")')
     mel.eval('dR_DoCmd("selectModeMarquee")')
 elif cmds.selectMode(q=True,o=True):
     cmds.selectMode(co=True)
     cmds.selectType(pv=True)
+    mel.eval('setToolTo $gMove')
     mel.eval('dR_DoCmd("selectModeTweakMarquee")')
+    mel.eval('dR_setActiveTransformAxis 3;')
+
+
+# -----------------------------------------------------
+# mel command
+# dR_setActiveTransformAxis 0;//x
+# dR_setActiveTransformAxis 1;//y
+# dR_setActiveTransformAxis 2;//z
+# dR_setActiveTransformAxis 3;//xyz
+# dR_setActiveTransformAxis 4;//xy
+# dR_setActiveTransformAxis 5;//yz
+# dR_setActiveTransformAxis 6;//xz
